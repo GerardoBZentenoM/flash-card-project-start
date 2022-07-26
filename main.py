e7119aa4d4e59c2cddaf5c9a0f10acc66cf44d11
GerardoBZentenoM <gerardo.zenteno@cyttek.com>
2022-07-26 13:59:13 -0500

    final commit

--- main.py
+++ main.py
@@ -1,66 +1,71 @@
-import tkinter
+from tkinter import *
+import pandas
 import random
-from pandas import read_csv
+
 BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
+to_learn = {}
+
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    print(original_data)
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
 
 
-window = tkinter.Tk()
-
-data = read_csv("data/french_words.csv")
-to_learn = data.to_dict(orient="records")
-
-
-def randow_french_word():
-    random_word = random.choice(to_learn)
-    canvas.itemconfigure(
-        word_canvas, text=random_word["French"], fill=BACKGROUND_COLOR)
-    canvas.itemconfigure(title_canvas, text="French", fill=BACKGROUND_COLOR)
-    canvas.itemconfig(french_image, image=my_image)
-    window.after(ms=3000, func=change_image)
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
 
 
-def change_image():
-    random_word = random.choice(to_learn)
-    # french_word =
-    canvas.itemconfig(french_image, image=new_image)
-    canvas.itemconfigure(
-        word_canvas, text=random_word["French"], fill="#FFFFFF")
-    canvas.itemconfigure(title_canvas, text="English", fill="#FFFFFF")
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
 
 
+def is_known():
+    to_learn.remove(current_card)
+    print(len(to_learn))
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
+
+
+window = Tk()
 window.title("Flashy")
-window.config(padx=50, pady=50)
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, func=flip_card)
 
-window.config(background=BACKGROUND_COLOR)
-canvas = tkinter.Canvas(width=800, height=526, highlightthickness=0)
-
-my_image = tkinter.PhotoImage(file="images/card_front.png")
-french_image = canvas.create_image(400, 263, image=my_image)
-
-new_image = tkinter.PhotoImage(file="images/card_back.png")
-
-title_canvas = canvas.create_text(
-    400, 150, text="Title", font=("Ariel", 40, "italic"))
-
-word_canvas = canvas.create_text(
-    400, 263, text="Word", font=("Ariel", 60, "bold"))
-
+canvas = Canvas(width=800, height=526)
+card_front_img = PhotoImage(file="images/card_front.png")
+card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
+card_title = canvas.create_text(
+    400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
-canvas.grid(column=0, row=0, columnspan=3, rowspan=2)
+canvas.grid(row=0, column=0, columnspan=2)
 
+cross_image = PhotoImage(file="images/wrong.png")
+unknown_button = Button(
+    image=cross_image, highlightthickness=0, command=next_card)
+unknown_button.grid(row=1, column=0)
 
-ok_logo = tkinter.PhotoImage(file="images/right.png")
-button = tkinter.Button(
-    image=ok_logo, highlightthickness=0,  command=randow_french_word)
-button.grid(column=0, row=2)
+check_image = PhotoImage(file="images/right.png")
+known_button = Button(
+    image=check_image, highlightthickness=0, command=is_known)
+known_button.grid(row=1, column=1)
 
-wrong_logo = tkinter.PhotoImage(file="images/wrong.png")
-button = tkinter.Button(
-    image=wrong_logo, highlightthickness=0, command=randow_french_word)
-button.grid(column=2, row=2)
-
-randow_french_word()
-
+next_card()
 
 window.mainloop()
